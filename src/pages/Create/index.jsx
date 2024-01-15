3396c830c40c655f074fd245f2ec5da31aee8ca1
xandguima <xan.96@hotmail.com>
2024-01-15 09:44:46 -0300

    capturing textMovie user

--- src/pages/Create/index.jsx
+++ src/pages/Create/index.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { api } from '../../services/api'
 import { FiArrowLeft } from 'react-icons/fi'
 
-import { Link } from 'react-router-dom'
 
 import{ContainerCreate,Content} from './style'
 
@@ -12,35 +14,102 @@ import {Textarea} from '../../components/Textarea'
 import {MarkersMovie} from '../../components/MarkersMovie'
 
 export function Create(){
+
+  const [title,setTitle]=useState('')
+  const [score,setScore]=useState('')
+  const [description,setDescription]=useState('')
+
+  const [marks,setMarks]= useState([]);
+  const [newMarks,setNewMarks]= useState('');
+
+  const navigate = useNavigate();
+  
+
+  function handleAddMarks(){
+    setMarks(prevState=>[newMarks,...prevState]);
+    setNewMarks('');
+  }
+
+  function handleRemoveMarks(deleted){
+    setMarks(prevState=>prevState.filter(mark=>mark!==deleted));
+  }
+  
+  async function handleNewMovie(){
+    if(!title || !description || !score || marks.length<1){
+      return alert("Preencha todos os campos")
+    }
+    if(newMarks){
+      return alert("Marcador em aberto, adicione o marcador em aberto ou apague")
+     
+    }
+    await api.post("/notes",{
+      title,
+      description,
+      score,
+      tags:marks
+    })
+
+    alert("Nota criada com sucesso!")
+
+    navigate("/");
+  }
+
   return(
     <ContainerCreate>
       <Header/>
-      <Content>
-      
+      <Content>  
         <ButtonText  icon={FiArrowLeft} title="Voltar"/>
-     
-       
         <form>
           <h1>Novo filme</h1>
           <section>
-            <Input placeholder="Título"/>
-            <Input placeholder="Sua nota (de 0 a 5)"/>
+            <Input 
+            placeholder="Título"
+            onChange={e=>setTitle(e.target.value)}
+            />
+            <Input 
+            placeholder="Sua nota (de 0 a 5)"
+            onChange={e=>setScore(e.target.value)}
+            onKeyPress={(event) => {
+              // Allow only numbers
+              if (!/[0-5]/.test(event.key)) {
+                event.preventDefault();
+              }
+            }
+            }
+            maxLength={1}
+            />
           </section>
           <div className='textArea'>
-            <Textarea placeholder="Observações"/>
+            <Textarea 
+            placeholder="Observações"
+            onChange={e=>setDescription(e.target.value)}
+            />
           </div>
           
           <div className='markers'>
             <h3>Marcadores</h3>
-            <section>
-              <MarkersMovie value="Drama"/>
-              <MarkersMovie isNew placeholder="Novo marcador"/>
+            <section> 
+              <MarkersMovie 
+              isNew 
+              placeholder="Novo marcador"
+              value={newMarks}
+              onChange={e=>setNewMarks(e.target.value)}
+              onClick={handleAddMarks}
+              />
+              {marks.map((mark,index)=>(
+                <MarkersMovie
+                key={String(index)}
+                value={mark}
+                onClick={()=>handleRemoveMarks(mark)}
+                />
+              ))}
+             
             </section>
           </div>
-          <div className='buttons'>
-            <Button isBlack title="Excluir filme"/>
-            <Button title="Salvar alterações"/>
-          </div>
+          
+          
+          <Button title="Salvar alterações" onClick={handleNewMovie}/>
+          
 
         </form>
         
