f5f57d783267abf6f85f380b012a0b4858adc73e
xandguima <xan.96@hotmail.com>
2024-01-15 09:36:25 -0300

    update main routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from './style/global'
 import theme from './style/theme'
-
+import {AuthProvider} from './hooks/auth'
 import { Routes } from './routes'
 
 
@@ -12,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode > 
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Routes/>
+      <AuthProvider>
+        <Routes/>
+      </AuthProvider>
     </ThemeProvider>
    
   </React.StrictMode>,
