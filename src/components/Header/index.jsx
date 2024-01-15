40fcd37dc190ad8e6d8818caad5527f47454ed21
xandguima <xan.96@hotmail.com>
2024-01-15 09:40:18 -0300

    capturing search of user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import avatarPlaceHolder from '../../assets/avatar_placeholder.svg'
+
 import { ContainerHeader,Profile } from "./style";
 import {Input} from '../../components/Input'
+import { Link } from 'react-router-dom';
+import { useAuth } from "../../hooks/auth";
 export function Header(){
+
+  const [search,setSearch] = useState('');
+
+
+
+  const {signOut,user} = useAuth();
+  const avatarUrl = user.avatar_url ? `${api.defaults.baseURL}/files/${user.avatar_url}` : avatarPlaceHolder
+
   return(
     <ContainerHeader>
       <h1>RocketMovies</h1>
-      <Input placeholder="Pesquisar pelo título"/>
-      <Profile to ="/profile">
+      
+      <Input 
+      placeholder="Pesquisar pelo título"
+      onChange={()=>setSearch(e.target.value)}
+
+      />
+      <Profile>
         <div>
-          <strong>Alexandre Guimarães</strong>
-          <a href="http:/">sair</a>
+          <Link id="linkStrong" to='/profile'><strong>Alexandre Guimarães</strong></Link> 
+          <Link to='/' onClick={signOut}>sair</Link>
         </div>
-        <img src="https://github.com/xandguima.png" alt="imagem de perfil"/>
+        <Link id="linkImg" to="/profile"><img src={avatarUrl} alt="imagem de perfil"/></Link>
       </Profile>
     </ContainerHeader>
   )
