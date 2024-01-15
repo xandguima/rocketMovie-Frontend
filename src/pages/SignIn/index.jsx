4534e2f400756deca39f55f614dda999f63ed264
xandguima <xan.96@hotmail.com>
2024-01-15 09:47:24 -0300

    verify user

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { FiMail,FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-import { ContainerSignIn,Form,Background } from "./style";
 
+import { ContainerSignIn,Form,Background } from "./style";
+import { useAuth } from "../../hooks/auth";
 import { Input } from "../../components/Input";
 import {Button} from "../../components/Button";
 
 export function SignIn() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const {signIn} = useAuth();
+
+  function handleSignIn(){
+    signIn({email,password});
+  }
 
   return (
     <ContainerSignIn>
@@ -15,9 +25,17 @@ export function SignIn() {
         <p>Aplicação para acompanhar tudo que assistir.</p>
 
         <h2>Faça seu login</h2>
-        <Input type="text"icon={FiMail}placeholder="E-mail"/>
-        <Input type="password"icon={FiLock}placeholder="Senha"/>
-        <Button title="Entrar"/>
+        <Input type="text"
+        icon={FiMail}
+        placeholder="E-mail"
+        onChange={e=>setEmail(e.target.value)}/>
+
+        <Input type="password"
+        icon={FiLock}
+        placeholder="Senha"
+        onChange={e=>setPassword(e.target.value)}/>
+
+        <Button title="Entrar" onClick={handleSignIn}/>
         <Link to='/register'>
           Criar conta
         </Link>
