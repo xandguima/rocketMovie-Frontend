fbc53af2329a789a3ded8576c86b6b1bd8926c45
xandguima <xan.96@hotmail.com>
2024-01-15 22:47:21 -0300

    extract search to send home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
-import { api } from "../../services/api";
-import avatarPlaceHolder from '../../assets/avatar_placeholder.svg'
 
-import { ContainerHeader,Profile } from "./style";
-import {Input} from '../../components/Input'
-import { Link } from 'react-router-dom';
 import { useAuth } from "../../hooks/auth";
-export function Header(){
+import { useState,useEffect } from "react";
+import { Link,useNavigate } from 'react-router-dom';
 
+import { api } from "../../services/api";
+import {Input} from '../../components/Input'
+import { ContainerHeader,Profile } from "./style";
+import avatarPlaceHolder from '../../assets/avatar_placeholder.svg'
+export function Header({onSearch=()=>{}}){
   const [search,setSearch] = useState('');
-
-
-
   const {signOut,user} = useAuth();
+  const navigate=useNavigate()
+
   const avatarUrl = user.avatar_url ? `${api.defaults.baseURL}/files/${user.avatar_url}` : avatarPlaceHolder
+
+  async function handleSearch(){
+    navigate("/");
+    signOut();
+  }
+  
+  useEffect(()=>{
+   onSearch(search);
+  },[search])
+
 
   return(
     <ContainerHeader>
@@ -22,12 +31,12 @@ export function Header(){
       <Input 
       placeholder="Pesquisar pelo título"
       onChange={()=>setSearch(e.target.value)}
-
       />
+
       <Profile>
         <div>
-          <Link id="linkStrong" to='/profile'><strong>Alexandre Guimarães</strong></Link> 
-          <Link to='/' onClick={signOut}>sair</Link>
+          <Link id="linkStrong" to='/profile'><strong>{user.name}</strong></Link> 
+          <Link to='/' onClick={handleSearch}>sair</Link>
         </div>
         <Link id="linkImg" to="/profile"><img src={avatarUrl} alt="imagem de perfil"/></Link>
       </Profile>
